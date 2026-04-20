c8329a4e1b7b178bdac81a7c19eab307fcb339ad
GoshiHayashi <goshi.hayashi@gmail.com>
2026-04-20 14:07:10 +0700

    feat: add terraform configuration for networking

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "Southeast Asia"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+
+  name                = "devops-aks"
+  location            = azurerm_resource_group.rg.location
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  dns_prefix          = "devopsaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
+
